a3ad5a8fd90a24d268f5fba8c09a117a09a067b3
LilJack118 <78555188+LilJack118@users.noreply.github.com>
2021-09-03 10:27:15 +0200

    django_course

--- projects/models.py
+++ projects/models.py
@@ -1,10 +1,13 @@
 from enum import unique
 from django.db import models
 import uuid
+from users.models import Profile
 
 # Create your models here.
 
 class Project(models.Model):
+    #ForeignKey makes possible one to many relationship
+    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length = 200)
     description = models.TextField(null = True, blank = True)
     featured_image = models.ImageField(null= True, blank=True, default="default.jpg")
