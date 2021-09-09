fd2c15f82e571e31c1abdb7c3222b0cf92392ea5
LilJack118 <78555188+LilJack118@users.noreply.github.com>
2021-09-09 20:31:01 +0200

    django_course

--- frontend (django_course)/main.js
+++ frontend (django_course)/main.js
@@ -11,6 +11,7 @@ let getProject = () => {
 
 let buildProjects = (projects) => {
     let projectsWrapper = document.getElementById('projects--wrapper')
+    projectsWrapper.innerHTML = ``
 
     for (let i = 0; projects.length > i; i++) {
         let project = projects[i]
@@ -21,8 +22,8 @@ let buildProjects = (projects) => {
                 <div>
                     <div class="card--header">
                         <h3>${project.title}</h3>
-                        <strong class = "vote--option" >&#43;</strong>
-                        <strong class = "vote--option" >&#8722;</strong>
+                        <strong class="vote--option" data-vote="up" data-project="${project.id}">&#43;</strong>
+                        <strong class="vote--option" data-vote="down" data-project="${project.id}">&#8722;</strong>
                     </div>
 
                     <i>${project.vote_ratio}% Positive feedback</i>
@@ -31,7 +32,41 @@ let buildProjects = (projects) => {
 
             </div>
         `
+
         projectsWrapper.innerHTML += projectCard
+    }
+
+    // Add event listener
+    addVoteEvents()
+
+}
+
+let addVoteEvents = () => {
+    let voteBtn = document.getElementsByClassName('vote--option')
+
+    for (let i = 0; voteBtn.length > i; i++) {
+        voteBtn[i].addEventListener('click', (e) => {
+            let token = localStorage.getItem('token')
+            let vote = e.target.dataset.vote
+            let project = e.target.dataset.project
+
+            fetch(`http://127.0.0.1:8000/api/projects/${project}/vote/`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${token}`
+                    },
+                    body: JSON.stringify({
+                        'value': vote
+                    })
+                })
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    getProject()
+                })
+
+        })
     }
 }
 
