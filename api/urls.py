8dfb45f48bf7315aa1db4050127058d923149a1e
LilJack118 <78555188+LilJack118@users.noreply.github.com>
2021-09-09 15:22:32 +0200

    django_course

--- api/urls.py
+++ api/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('', views.getRoutes),
     path('projects/', views.getProjects),
     path('projects/<str:pk>/', views.getProject),
+    path('projects/<str:pk>/vote/', views.projectVote),
 ]
--- django_course/settings.py
+++ django_course/settings.py
@@ -79,7 +79,7 @@ SIMPLE_JWT = {
     'JTI_CLAIM': 'jti',
 
     'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=10),
+    'SLIDING_TOKEN_LIFETIME': timedelta(days=1),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 
